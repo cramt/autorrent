4b345f6b7aa9a8ab7d040382e223b23470e7d35c
mads østermark <madscramt@gmail.com>
2018-11-07 23:14:58 +0100

    a

--- autorrent/Program.cs
+++ autorrent/Program.cs
@@ -2,14 +2,23 @@
 using Chromely.CefGlue.Winapi.ChromeHost;
 using Chromely.Core;
 using Chromely.Core.Helpers;
+using Chromely.Core.Infrastructure;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
 using WinApi.Windows;
 
 namespace autorrent {
     class Program {
         static int Main(string[] args) {
-            string startUrl = "https://www.google.com/";
-
+           
+            WebpackBindings webpackBindings = new WebpackBindings(MyWebpackBindingsStandarts.FrontendPath, MyWebpackBindingsStandarts.WebpackStartCommand);
+            webpackBindings.WaitForFirstBuild.Wait();
+            //string startUrl = "local://" + String.Join('/', Path.Join(MyWebpackBindingsStandarts.FrontendPath, "dist", "index.html").Split('\\'));
+            string startUrl = "local://index.html";
+            Temp t = new Temp();
             ChromelyConfiguration config = ChromelyConfiguration
                                           .Create()
                                           .WithAppArgs(args)
@@ -19,12 +28,16 @@ namespace autorrent {
 
                                           // Alternate approach for multi-process, is to add a subprocess application
                                           //.WithCustomSetting(CefSettingKeys.BrowserSubprocessPath, path_to_sunprocess)
-                                          .WithCustomSetting(CefSettingKeys.SingleProcess, true)
+                                          .WithLogFile("logs\\chromely.cef_new.log")
+                                          .WithLogSeverity(LogSeverity.Info)
+                                              .UseDefaultLogger()
+                                          //.WithCustomSetting(CefSettingKeys.SingleProcess, true)
+                                          .RegisterJsHandler("temp", t, null, false)
+                                          .UseDefaultResourceSchemeHandler("local", string.Empty)
                                           .WithStartUrl(startUrl);
-
             var factory = WinapiHostFactory.Init();
             using (var window = factory.CreateWindow(() => new CefGlueBrowserHost(config),
-                  "chromely", constructionParams: new FrameWindowConstructionParams())) {
+                  "autorrent", constructionParams: new FrameWindowConstructionParams())) {
                 window.SetSize(config.HostWidth, config.HostHeight);
                 window.CenterToScreen();
                 window.Show();
---
+++ autorrent/Temp.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace autorrent {
+    public class Temp {
+        public Action F = null;
+    }
+}
