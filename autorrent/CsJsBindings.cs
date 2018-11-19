343fd13dce57e4c57c8d98b74739c017b589b7cc
mads østermark <madscramt@gmail.com>
2018-11-19 22:37:29 +0100

    stuff

--- autorrent/CsJsBindings.cs
+++ autorrent/CsJsBindings.cs
@@ -17,31 +17,41 @@ namespace autorrent {
                 object obj = propertyDescriptor.GetValue(dynObj);
                 dictionary.Add(propertyDescriptor.Name, obj);
             }
+
             return dictionary;
         }
     }
+
     public class CsJsBindings {
         private ChromeForm chromeForm;
+
         public CsJsBindings(ChromeForm chromeForm) {
             this.chromeForm = chromeForm;
         }
+
         public void Log(object o) {
             try {
-                dynamic d = (dynamic)o;
+                dynamic d = (dynamic) o;
                 Console.WriteLine("line: " + d.line);
                 Console.WriteLine(d.msg);
             }
-            catch (Exception) { }
+            catch (Exception) {
+                // ignored
+            }
         }
+
         public void WindowCaptureBorder() {
             chromeForm.CaptureBorder();
         }
+
         public void WindowReleaseBorder() {
             chromeForm.ReleaseBorder();
         }
+
         public void WindowClose() {
             Application.Exit();
         }
+
         public void WindowToggleMaximize() {
             chromeForm.Invoke(new Action(() => {
                 if (chromeForm.WindowState == FormWindowState.Normal) {
@@ -52,13 +62,14 @@ namespace autorrent {
                 }
             }));
         }
+
         public void WindowMinimize() {
-            chromeForm.Invoke(new Action(() => {
-                chromeForm.WindowState = FormWindowState.Minimized;
-            }));
+            chromeForm.Invoke(new Action(() => { chromeForm.WindowState = FormWindowState.Minimized; }));
         }
+
         public int TorrentInitFromMagnetLink(string magnetLink) {
-            TorrentSession torrent = new TorrentClient(@"C:\Users\madsc\Downloads").InitFromMagnetLink(MagnetLink.Parse(magnetLink)).GetAwaiter().GetResult();
+            TorrentSession torrent = new TorrentClient(@"C:\Users\madsc\Downloads")
+                .InitFromMagnetLink(MagnetLink.Parse(magnetLink)).GetAwaiter().GetResult();
             return 0;
         }
     }
--- autorrent/Program.cs
+++ autorrent/Program.cs
@@ -1,44 +1,20 @@
-using au.Torrent;
 using CefSharp;
 using CefSharp.WinForms;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 #pragma warning disable CS4014 
 
 namespace autorrent {
     class Program {
-        static async Task Test() {
-            MagnetLink m = MagnetLink.Parse("magnet:?xt=urn:btih:b88c7d3fb38c305350d064a83517d18d564a609e&dn=The.Flash.2014.S05E04.News.Flash.1080p.AMZN.WEBRip.DDP5.1.x264-NTb%5Brartv%5D&tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2710&tr=udp%3A%2F%2F9.rarbg.to%3A2710");
-            var session = await new TorrentClient(@"C:\Users\madsc\Downloads").InitFromMagnetLink(m);
-            float? piecesLength = null;
-            session.Metainfo.ContinueWith(x => {
-                piecesLength = x.Result.Pieces.Length;
-            });
-            float pieceIndex = 0;
-            session.PieceCompleted += (object sender, PieceCompletedEventArgs e) => {
-                pieceIndex++;
-                if (piecesLength != null) {
-                    Console.WriteLine(pieceIndex/piecesLength);
-                }
-            };
-            await session.DataCompletion;
-            Console.WriteLine("done");
-        }
 
-        const bool doTheWebpack = false;
-        static void Main(string[] args) {
+        const bool DoTheWebpack = false;
+
+        private static void Main(string[] args) {
             CefSettings settings = new CefSettings();
             Cef.EnableHighDPISupport();
             Cef.Initialize(settings);
-            //Test();
-            //Thread.Sleep(-1);
             ChromeForm mainForm = new ChromeForm(MyWebpackBindingsStandards.DistIndexHtml);
-            if (doTheWebpack) {
+            if (DoTheWebpack) {
                 WebpackBindings bindings = new WebpackBindings(MyWebpackBindingsStandards.FrontendPath, MyWebpackBindingsStandards.WebpackStartCommand);
                 Console.WriteLine("starting webpack");
                 bindings.WaitForFirstBuild.Wait();
